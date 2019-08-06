457002630f6b5c7000ecbddeb1b029b693d2c3d3
Derik <derikhill05@gmail.com>
2019-08-06 16:44:25 -0500

    A few layout tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,19 @@ class About extends React.Component {
             <div>
                 <Header />
                 <div className="aboutDisplay">
-                    <div>3 columns</div>
-                    <div>3 columns</div>
-                    <div>3 columns</div>
+                {/* <img src='http://loremflickr.com/g/250/150/boat' alt=''/> */}
+                    <div className="cell column-1">
+                        <h2>Column 1</h2>
+                        <p>church-key pork belly Portland direct trade  hoodie polaroid before they sold out photo booth Marfa tousled mixtape deep v Cosby sweater readymade kogi aesthetic axe PBR&B meh farm-to-table selvage post-ironic quinoa</p>
+                    </div>
+                    <div className="cell column-2">
+                        <h2>COLMUN 2</h2>
+                        <p>Intelligentsia Godard synth kale chips Neutra gluten-free High Life banh mi mixtape whatever tofu vinyl cleanse post-ironic authentic cray Portland retro Austin Williamsburg banjo Echo Park crucifix pour-over readymade</p>
+                    </div>
+                    <div className="cell column-3">
+                        <h2>COLMUN 3</h2>
+                        <p>tousled beard scenester sartorial Marfa swag McSweeney's hashtag twee Truffaut chambray direct trade  four loko crucifix selfies retro biodiesel umami pug Williamsburg authentic 3 wolf moon iPhone Tonx raw denim quinoa</p>
+                    </div>
                 </div>
             </div>
         )
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-// import logo from '../css/logo.svg';
+import logo from '../css/logo.svg';
 import '../css/App.scss';
 
 class App extends React.Component {
@@ -8,23 +8,23 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header className="App-header"></Header>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
       </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
+      </div>
     )
   }
 }
